705784a0676fb24539de2c88db9d47db7fb8c7f2
Masaaki Komori <gmgateway@gmail.com>
2025-12-18 03:06:07 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
-import { SquareDashed } from 'lucide-react'
+import { CodeSquare } from 'lucide-react'
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-100 min-h-screen">
-      <div className="flex items-center gap-2 mt-4">
-        <SquareDashed className="w-6 h-6" />
-        <h1 className="font-bold text-2xl">Hello world!</h1>
+    <div className="flex flex-col justify-center items-center bg-white min-h-screen">
+      <div className="flex flex-col items-start gap-1">
+        <div className="flex items-center gap-2">
+          <CodeSquare className="w-6 h-6" />
+          <h1 className="font-medium text-gray-900 text-xl">
+            Vite: React w/ Tailwind v4
+          </h1>
+        </div>
+        <p className="text-gray-600 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
     </div>
   )
