159e438862e83c45f6ab623ab588f99a3e5756a2
Masaaki Komori <gmgateway@gmail.com>
2026-01-09 18:28:03 +0900

    Update modules and comment base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  // base path for deploying to a subdirectory
+  // base: './',
+  // path aliases
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
